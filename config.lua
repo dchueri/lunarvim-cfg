9da5a16ca94310544e1fffed4b33a59f5212e8d8
diego <diegochueri@gmail.com>
2023-07-20 14:15:42 -0300

    feat: add autotag

--- config.lua
+++ config.lua
@@ -3,6 +3,6 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
-lvim.plugins = {"nvim-neotest/neotest", "haydenmeade/neotest-jest"}
+lvim.plugins = {"windwp/nvim-ts-autotag","nvim-neotest/neotest", "haydenmeade/neotest-jest"}
 
 reload("user.options")
